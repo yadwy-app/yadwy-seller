8d3cfba584affe2ecf928298cbf8b95a3ba33037
Refaat <hassaan.refaat@taager.com>
2026-01-27 22:57:58 +0200

    update the server port

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
+	server: {
+		port: 3000,
+	},
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
